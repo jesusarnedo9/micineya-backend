967a14b4f2f357ec82b994186403cdac41fea400
jesusarnedo9 <97080@sistemas.frc.utn.edu.ar>
2026-08-13 17:52:43 -0300

    Initial commit

---
+++ micine/src/main/java/com/arnedo/micine/MicineApplication.java
@@ -0,0 +1,13 @@
+package com.arnedo.micine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicineApplication.class, args);
+	}
+
+}
---
+++ micine/src/test/java/com/arnedo/micine/MicineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arnedo.micine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
