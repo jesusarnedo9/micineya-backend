a56a125c64b35d19c18a8214f3804d1a92086ea8
jesusarnedo9 <97080@sistemas.frc.utn.edu.ar>
2026-08-25 23:20:06 -0300

    Fix Jackson ignorando campos de Peliculas

--- micine/src/main/java/com/arnedo/micine/dto/PeliculaDto.java
+++ micine/src/main/java/com/arnedo/micine/dto/PeliculaDto.java
@@ -1,7 +1,9 @@
 package com.arnedo.micine.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PeliculaDto {
     private Long id;
     private String title;
